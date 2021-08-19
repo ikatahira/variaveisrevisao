a8d1f5f90237aaf236dd1575e4e7c6253aabf6da
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-08-19 17:54:12 +0000

    acrescentei duas variáveis: gastosFevereiro e gastosMarco

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,15 +1,15 @@
 public class BalancoTrimestral{
     public static void main(String args[]){
         int gastosJaneiro=15000;
-        int gastosFevereiro
-        int gastosMarco
+        int gastosFevereiro;
+        int gastosMarco;
     }
 
 
 
 
     
-Crie também as variáveis gastosFevereiro e gastosMarco , inicializando-as com 23000 e 17000, respectivamente, utilize uma linha para cada declaração;
-Crie uma variável chamada gastosTrimestre e inicialize-a com a soma das outras 3 variáveis: int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
-Imprima a variável gastosTrimestre .
+//Crie também as variáveis gastosFevereiro e gastosMarco , inicializando-as com 23000 e 17000, respectivamente, utilize uma linha para cada declaração;
+//Crie uma variável chamada gastosTrimestre e inicialize-a com a soma das outras 3 variáveis: int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+//Imprima a variável gastosTrimestre .
 }
